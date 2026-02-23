b6048e4bc0e4774ead8753ec6741087f157dd9e4
Mafttah <posarobit@gmail.com>
2026-02-23 22:54:42 +0000

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -7,5 +7,18 @@ def on_forever():
         json_str = '{"l":"' + str(light_level) + '","t":' + (temp) + "}"
         basic.show_icon(IconNames.DIAMOND)
         basic.pause(3000)
+
+def calculate_checksum(text: str):
+    checksum = 0
+    ascii_chars = " !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~"
+    for ch in text:
+        code = 32
+        for i in range(len(ascii_chars)):
+            if ascii_chars[i] == ch:
+                code = 32 + i
+                break
+        checksum = (checksum + code) % 256
+    hex_chars = "0123456789ABCDEF"
+    return hex_chars[checksum // 16] + hex_chars[checksum % 16]
         
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,3 +1,22 @@
+function calculate_checksum(text: string) {
+    let code: number;
+    let checksum = 0
+    let ascii_chars = " !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~"
+    for (let ch of text) {
+        code = 32
+        for (let i = 0; i < ascii_chars.length; i++) {
+            if (ascii_chars[i] == ch) {
+                code = 32 + i
+                break
+            }
+            
+        }
+        checksum = (checksum + code) % 256
+    }
+    let hex_chars = "0123456789ABCDEF"
+    return hex_chars[Math.idiv(checksum, 16)] + hex_chars[checksum % 16]
+}
+
 basic.forever(function on_forever() {
     function on_button_pressed_a() {
         //  Change this - each group uses different number!
